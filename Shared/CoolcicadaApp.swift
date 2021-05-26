a7037eaadcc324dbfa27f163e05665063905bcd9
Abdullah Ridwan <abdullahridwan73@gmail.com>
2021-05-25 21:58:05 -0400

    Initial Commit

---
+++ Shared/CoolcicadaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolcicadaApp.swift
+//  Shared
+//
+//  Created by Abdullah Ridwan on 5/25/21.
+//
+
+import SwiftUI
+
+@main
+struct CoolcicadaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
